d9f42bcdd64e8783f805980e9c58e830388ce947
Joshua Daniel <joshua_dup@hotmail.com>
2024-06-04 17:23:35 -0500

    primer commit, estructura back

---
+++ src/main/java/com/verdevia/backend/VerdeviaApplication.java
@@ -0,0 +1,13 @@
+package com.verdevia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdeviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdeviaApplication.class, args);
+	}
+
+}
