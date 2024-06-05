c6aa7e7e34639a5c4cabd9436d560c0006d2580e
Joshua Daniel <joshua_dup@hotmail.com>
2024-06-04 19:48:02 -0500

    model departamento

---
+++ src/main/java/com/verdevia/backend/models/Departamento.java
@@ -0,0 +1,22 @@
+package com.verdevia.backend.models;
+
+public class Departamento {
+    private Long id_departamento;
+    private String nombre;
+
+    
+    public Long getId_departamento() {
+        return id_departamento;
+    }
+    public void setId_departamento(Long id_departamento) {
+        this.id_departamento = id_departamento;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+
