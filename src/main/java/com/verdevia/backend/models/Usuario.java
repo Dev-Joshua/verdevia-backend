1a24804985512dfc9baf98ad292f4b0430321f57
Joshua Daniel <joshua_dup@hotmail.com>
2024-06-04 19:41:03 -0500

    creo model usuario

--- src/main/java/com/verdevia/backend/models/Usuario.java
+++ src/main/java/com/verdevia/backend/models/Usuario.java
@@ -1,5 +1,43 @@
 package com.verdevia.backend.models;
 
 public class Usuario {
+    private Long id_usuario;
+    private String nombre;
+    private String apellidos;
+    private String correo_electronico;
+    private String contrasena;
 
+
+
+    public Long getId_usuario() {
+        return id_usuario;
+    }
+    public void setId_usuario(Long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
 }
